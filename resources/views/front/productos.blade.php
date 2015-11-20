59d343abeeabafaf64d04e5862e94b92d750cda1
mauriruizd <mauri.rd@gmail.com>
2015-11-19 22:40:47 -0300

    cambios en el scroll de los productos

--- resources/views/front/productos.blade.php
+++ resources/views/front/productos.blade.php
@@ -3,134 +3,88 @@
     # Productos
 @stop
 @section('contenido')
-    <div class="contenedor_pro">
-        <div class="menu_delos_prod">
+    @if(count($productos) > 0)
+        <div class="contenedor_pro">
+            <div class="menu_delos_prod">
 
-            <ul>
-                <li><a href="#section1" class="section1"> ► uno</a></li>
-                <li><a href="#section2" class="section2"> ► dos</a></li>
-                <li><a href="#section3" class="section3"> ► dos</a></li>
+                <ul>
+                    @foreach($productos as $producto)
+                        <li>
+                            <a href="#{{ $producto->slug }}">  ► {{ $producto->nombre }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+            <div class="foto_produc">
+                @foreach($productos as $producto)
+                    <div class="titulo_prod"><h3><img src="{{ URL::to('img/flecha.png') }}"> {{ $producto->nombre }}</h3></div>
+                    <div class="container" id="{{ $producto->slug }}">
+                        <div class="col-md-8" >
+                            <div class="col-md-4 fo">
+                                <img src="{{ count($producto->imagenes) > 0 ? URL::to($producto->imagenes[0]->img_url) : '' }}">
+                            </div>
+                            <div class="col-md-4 fo1"><!--datos de foto-->
+                                <div class="cuadr"><h4>• EPOCA</h4></div>
+                                <div class="cuadr1"><h4>• CICLO PROMEDIO</h4></div>
+                                <div class="cuadr1"><h4>• SEGMENTO</h4></div>
+                                <div class="cuadr1"><h4>• ENVASE</h4></div>
+                                <div class="cuadr1"><h4>• PROVEDOR</h4></div>
 
-            </ul>
-        </div>
-        <div class="foto_produc">
-            <div class="titulo_prod"><h3><img src="img/flecha.png"> Itatiba II</h3></div>
-            <div class="container" id="section1">
-                <div class="col-md-8" >
-                    <div class="col-md-4 fo">
-                        <img src="img/producto.jpg">
-                    </div>
-                    <div class="col-md-4 fo1"><!--datos de foto-->
-                        <div id="cuadr"><h4>• EPOCA1</h4></div>
-                        <div id="cuadr1"><h4>• CICLO PROMEDIO</h4></div>
-                        <div id="cuadr1"><h4>• SEGMENTO</h4></div>
-                        <div id="cuadr1"><h4>• ENVASE</h4></div>
-                        <div id="cuadr1"><h4>• PROVEDOR</h4></div>
+                            </div><!--datos de foto-->
 
-                    </div><!--datos de foto-->
+                            <div class="col-md-4 fo1"><!--datos de foto-->
+                                <div class="cuadr"><h4> {{ $producto->epoca }}</h4></div>
+                                <div class="cuadr1"><h4> {{ $producto->ciclo_promedio }}</h4></div>
+                                <div class="cuadr1"><h4> {{ $producto->segmento }}</h4></div>
+                                <div class="cuadr1"><h4> {{ $producto->envase }}</h4></div>
+                                <div class="cuadr1">
+                                    <ol>
+                                        @foreach($producto->proveedorProducto as $prov)
+                                            <li>{{ $prov->proveedor->nombre }}</li>
+                                        @endforeach
+                                    </ol>
+                                </div>
 
-                    <div class="col-md-4 fo1"><!--datos de foto-->
-                        <div id="cuadr"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
+                            </div><!--datos de foto-->
 
-                    </div><!--datos de foto-->
+                        </div>
 
-                </div>
+                        <div class="col-md-8">
+                            <div class="carac">
+                                <h4>• CARACTERISTICAS GENERALES</h4>
+                            </div>
 
-                <div class="col-md-8">
-                    <div class="carac">
-                        <h4>• CARACTERISTICAS GENERALES</h4>
-                    </div>
+                            <div class="carac1">
+                                {{ $producto->descripcion }}
+                            </div>
 
-                    <div class="carac1">
-                    </div>
+                        </div>
+                    </div><!--container-->
+                @endforeach
+            </div><!--foto_produc-->
+        </div><!--contenedor_pro-->
+    @else
+        <h1>No hay productos en esta subcategoria</h1>
+    @endif
+@stop
+@section('includes')
+    <script src="{{ URL::to('js/scrollto.js') }}"></script>
+    <script>
+        $(document).ready(function(){
 
-                </div>
-            </div><!--container-->
+            $('.menu_delos_prod:first li').click(function(e) {
+                e.preventDefault();
+                var id = $(this).children('a').attr('href');
+                var elm = $(id).prev();
+                console.log(elm);
+                $('.foto_produc:first').scrollTo(elm, 5);
+                /*var prod_cont = $('.foto_produc:first');
+                var pos = prod_cont.scrollTop() + elm.position().top - 5;
 
-            <br>
-            <div class="titulo_prod"><h3><img src="img/flecha.png"> Itatiba II</h3></div>
-
-            <div class="container"id="section2" >
-                <div class="col-md-8">
-                    <div class="col-md-4 fo">
-                        <img src="img/producto.jpg">
-                    </div>
-                    <div class="col-md-4 fo1"><!--datos de foto-->
-                        <div id="cuadr"><h4>• EPOCA2</h4></div>
-                        <div id="cuadr1"><h4>• CICLO PROMEDIO</h4></div>
-                        <div id="cuadr1"><h4>• SEGMENTO</h4></div>
-                        <div id="cuadr1"><h4>• ENVASE</h4></div>
-                        <div id="cuadr1"><h4>• PROVEDOR</h4></div>
-
-                    </div><!--datos de foto-->
-
-                    <div class="col-md-4 fo1"><!--datos de foto-->
-                        <div id="cuadr"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-
-                    </div><!--datos de foto-->
-
-                </div>
-
-
-
-                <div class="col-md-8">
-                    <div class="carac">
-                        <h4>• CARACTERISTICAS GENERALES</h4>
-                    </div>
-
-                    <div class="carac1">
-                    </div>
-
-                </div>
-            </div><!--container-->
-
-            <div class="titulo_prod"><h3><img src="img/flecha.png"> Itatiba II</h3></div>
-            <div class="container"id="section3" >
-                <div class="col-md-8">
-                    <div class="col-md-4 fo">
-                        <img src="img/producto.jpg">
-                    </div>
-                    <div class="col-md-4 fo1"><!--datos de foto-->
-                        <div id="cuadr"><h4>• EPOCA2</h4></div>
-                        <div id="cuadr1"><h4>• CICLO PROMEDIO</h4></div>
-                        <div id="cuadr1"><h4>• SEGMENTO</h4></div>
-                        <div id="cuadr1"><h4>• ENVASE</h4></div>
-                        <div id="cuadr1"><h4>• PROVEDOR</h4></div>
-
-                    </div><!--datos de foto-->
-
-                    <div class="col-md-4 fo1"><!--datos de foto-->
-                        <div id="cuadr"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-                        <div id="cuadr1"><h4> datos</h4></div>
-
-                    </div><!--datos de foto-->
-
-                </div>
-
-
-
-                <div class="col-md-8">
-                    <div class="carac">
-                        <h4>• CARACTERISTICAS GENERALES</h4>
-                    </div>
-
-                    <div class="carac1">
-                    </div>
-
-                </div>
-            </div><!--container-->
-
-        </div><!--foto_produc-->
-    </div><!--contenedor_pro-->
+                prod_cont.animate({
+                    scrollTop : pos
+                }, 400);*/
+            });
+        });
+    </script>
 @stop
