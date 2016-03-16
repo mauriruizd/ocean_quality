568315e75f55d55789e2f885d9f9f0af05f74739
Mauricio Ruiz Diaz Maciel <mauri.rd@gmail.com>
2016-03-16 10:15:32 -0300

    Agregado truncate en todos los seeders y quitado el seeder duplicado de categorias

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder {
 
@@ -13,18 +14,20 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Model::unguard();
-
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
 		// $this->call('UserTableSeeder');
 		$this->call('CategoriasTableSeeder');
 		$this->call('DepartamentosTableSeeder');
-		$this->call('CategoriasTableSeeder');
 		$this->call('AdminTableSeeder');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+		Model::reguard();
 	}
 
 }
 
 class DepartamentosTableSeeder extends Seeder {
 	public function run() {
+		DB::table('departamentos')->truncate();
 		$departamentos = ['Concepcion', 'San Pedro', 'Cordillera', 'Guaira', 'Caaguazu', 'Caazapa',
 		'Itapua', 'Misiones', 'Paraguari', 'Alto Parana', 'Central', 'Neembucu', 'Amambay', 'Canindeyu',
 		'Presidente Hayes', 'Boqueron', 'Alto Paraguay'];
@@ -37,6 +40,7 @@ class DepartamentosTableSeeder extends Seeder {
 
 class CategoriasTableSeeder extends Seeder {
 	public function run() {
+		DB::table('categorias')->truncate();
 		$categorias = ['Semillas', 'Fertilizantes', 'Sistemas de riego', 'Agroquímicos', 'Ambientes protegidos'];
 		foreach($categorias as $categoria) {
 			\App\Categoria::create(['nombre' => $categoria]);
@@ -46,6 +50,7 @@ class CategoriasTableSeeder extends Seeder {
 
 class AdminTableSeeder extends  Seeder {
 	public function run() {
+		DB::table('administradores')->truncate();
 		\App\Administrador::create([
 			'nombre' => 'Ocean',
 			'email' => 'admin@oceanquality.net',
